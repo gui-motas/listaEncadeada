a8562b00e014d227a85adbd64c84b85f62f6b8b8
Victor Liger <128982763+VicLiger@users.noreply.github.com>
2024-03-20 16:51:14 -0300

    Métodos testados

--- src/listaSimplesmenteEncadeada/listaEncadeada.java
+++ src/listaSimplesmenteEncadeada/listaEncadeada.java
@@ -3,8 +3,88 @@ package listaSimplesmenteEncadeada;
 public class listaEncadeada{
 
 public static void main(String[]args){
-	
-  }
+	Lista newList = new Lista();
 
+        System.out.println("Inserindo e mostrando itens");
+
+        newList.insert(5);
+        newList.insert(10);
+        newList.insert(15);
+        newList.insert(20);
+        newList.insert(25);
+
+        newList.showList();
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Retornando o ultimo item");
+
+        Item lastItem = newList.getLastItem();
+        float lastItemData = lastItem.data;
+        System.out.println(lastItemData);
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Buscando o item desejado");
+
+        Item findItem = newList.findItemAtIndex(0);
+        System.out.println(findItem.data);
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Inserindo o item no final");
+
+        newList.insertEnd(111);
+        newList.showList();
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Inserindo o item no meio");
+
+        newList.insertMiddle(222);
+        newList.showList();
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Escolhendo o item a ser removido");
+
+        newList.removeItemAtIdex( 4);
+        newList.showList();
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Exibindo o tamanho da lista");
+
+        int length = newList.getListLength();
+        System.out.println( length);
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Embaralhando a lista");
+
+        newList.sortList();
+        newList.showList();
+
+
+        System.out.println("--------------------------------------------------------");
+
+
+        System.out.println("Invertendo a ordem da lista");
+
+        newList.invertList();
+        newList.showList();
+    }
 }
-  
